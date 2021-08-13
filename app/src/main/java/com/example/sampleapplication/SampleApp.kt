fca49ea785be771d938b809599ae636980d44042
skliba <kliba.stefano@gmail.com>
2021-08-13 13:26:27 +0200

    Create Application class

---
+++ app/src/main/java/com/example/sampleapplication/SampleApp.kt
@@ -0,0 +1,12 @@
+package com.example.sampleapplication
+
+import android.app.Application
+import timber.log.Timber
+
+class SampleApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree)
+    }
+}
---
+++ app/src/main/java/com/example/sampleapplication/ui/search/SearchFragment.kt
@@ -0,0 +1,12 @@
+package com.example.sampleapplication.ui.search
+
+import com.example.sampleapplication.ui.base.BaseFragment
+
+class SearchFragment : BaseFragment() {
+
+    override val layoutResourceId: Int = R.layout.fragment_article_details
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
